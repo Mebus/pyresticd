3a6d76201104b928c1b9053317c9e61804814ff5
Thor77 <thor77@thor77.org>
2016-10-29 15:04:50 +0200

    Use py3-style print and string-formatting

--- pyresticd.py
+++ pyresticd.py
@@ -8,19 +8,21 @@ from twisted.internet import task
 from twisted.internet import reactor
 
 
-# Configuration 
+# Configuration
 
 timeout = 3600*24*3 # Period
 restic_command = "/home/mebus/restic" # your restic command here
 
 # Program
 
+
 def do_restic_backup():
-    print "\nStarting Backup at " + str(time.ctime())
+    print('Starting Backup at {}'.format(time.ctime()))
     os.system(restic_command)
 
-print "\nRestic Scheduler\n----------------------------\n"
-print "Timout ist: " + str(timeout)
+print('Restic Scheduler')
+print('-' * 30)
+print('Timeout: {}'.format(timeout))
 restic_password = getpass.getpass(prompt="Please enter the restic encryption password: ")
 os.environ["RESTIC_PASSWORD"] = restic_password
 
