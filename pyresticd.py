a00b6ca1eb486187160aa75cc219006b02b66e1f
Thor77 <thor77@thor77.org>
2016-10-29 22:31:19 +0200

    Use subprocess.Popen instead of os.system for command-running
    because it supports setting the env for that specific process.
    
    * Initialize restic_password at the top
    * Pass restic_password as arg to do_restic_backup

--- pyresticd.py
+++ pyresticd.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os
 import getpass
 import time
+from subprocess import Popen
 from twisted.internet import task
 from twisted.internet import reactor
 
@@ -11,22 +11,28 @@ from twisted.internet import reactor
 # Configuration
 
 timeout = 3600*24*3 # Period
-restic_command = "/home/mebus/restic" # your restic command here
+restic_executable = 'restic'
+restic_args = ''
+restic_password = ''
 
 # Program
 
 
-def do_restic_backup():
+def do_restic_backup(password):
     print('Starting Backup at {}'.format(time.ctime()))
-    os.system(restic_command)
+    args = [restic_executable] + restic_args.split()
+    Popen(args, env={
+        'RESTIC_PASSWORD': password
+    })
+
 
 print('Restic Scheduler')
 print('-' * 30)
 print('Timeout: {}'.format(timeout))
-restic_password = getpass.getpass(prompt="Please enter the restic encryption password: ")
-os.environ["RESTIC_PASSWORD"] = restic_password
+restic_password = getpass.getpass(
+    prompt='Please enter the restic encryption password: ')
 
-l = task.LoopingCall(do_restic_backup)
+l = task.LoopingCall(do_restic_backup, restic_password)
 l.start(timeout)
 
 reactor.run()
