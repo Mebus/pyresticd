250a62c60b8f2874d491ff36ffbef954851db647
Mebus <mebus.inbox@googlemail.com>
2017-04-21 21:32:44 +0200

    schedule & config file, some improvements

--- README.md
+++ README.md
@@ -4,6 +4,30 @@
 
 Simple Python Script to run [restic](https://restic.github.io/) in a Daemon like fashion.
 
+### Requirements
+
+python3, pip3
+
+### Installation
+
+Install Python requirements:
+
+<code>
+pip3 install schedule
+</code>
+
+Copy config file:
+
+<code>
+cp pyresticd.conf.exmaple pyresticd.conf
+</code>
+
+Edit pyresticd.conf
+
+### Usage
+
+Start it in tmux or screen.
+
 ### Author
 
 Mebus
--- pyresticd.py
+++ pyresticd.py
@@ -1,40 +1,79 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import getpass
-import time
 import os
-from subprocess import Popen
-
-from twisted.internet import reactor, task
+import time
+import getpass
+import schedule
+import configparser
+import subprocess
+import syslog
 
 # Configuration
 
-timeout = 3600 * 24 * 3  # Period
-restic_executable = 'restic'
 restic_args = ''
 restic_password = ''
 
+# Load Configuration
+
+config = configparser.ConfigParser()
+config.read("pyresticd.cfg")
+restic_command = config['pyresticd']['restic_command']
+backup_at = config['pyresticd']['backup_at']
+
 # Program
+
+def logthis(msg):
+
+    msg = msg + " at {}".format(time.ctime())
+
+    print(msg)
+    syslog.syslog(msg)
 
 
 def do_restic_backup(password):
-    print('Starting Backup at {}'.format(time.ctime()))
-    args = [restic_executable] + restic_args.split()
-    Popen(args, env={
+
+    # Log start
+    logthis('Starting pyresticd Backup')
+    
+    # run restic
+    args = [restic_command] + restic_args.split()
+
+    ps = subprocess.Popen(args, env={
         'RESTIC_PASSWORD': password,
         'PATH': os.environ['PATH'],
     })
 
+    ps.wait()
 
+    #Log finish
+    logthis('Finished pyresticd Backup')
+
+
+print('=' * 30)
 print('Restic Scheduler')
+print('=' * 30)
+
+print("restic command: " + restic_command)
+print("backup at: " + backup_at)
+
 print('-' * 30)
-print('Timeout: {}'.format(timeout))
+
 if not restic_password:
     restic_password = getpass.getpass(
         prompt='Please enter the restic encryption password: ')
+    print("Password entered.")
 
-l = task.LoopingCall(do_restic_backup, restic_password)
-l.start(timeout)
+# Immediate Backup
 
-reactor.run()
+answer = input('Run a backup just now? [y/n]')
+if answer and answer[0].lower() == 'y':
+    do_restic_backup(restic_password)
+
+# Scheduling
+
+schedule.every().day.at(backup_at).do(do_restic_backup, restic_password)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
