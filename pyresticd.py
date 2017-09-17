c964b741874fc85f449ff78318c00a91924a87ce
Mebus <mebus.inbox@googlemail.com>
2017-09-17 11:29:40 +0200

    bugfix day interval

--- pyresticd.py
+++ pyresticd.py
@@ -20,7 +20,7 @@ config = configparser.ConfigParser()
 config.read("pyresticd.cfg")
 restic_command = config['pyresticd']['restic_command']
 backup_at = config['pyresticd']['backup_at']
-day_interval = config['pyresticd']['day_interval']
+day_interval = int(config['pyresticd']['day_interval'])
 
 # Program
 
@@ -36,7 +36,7 @@ def do_restic_backup(password):
 
     # Log start
     logthis('Starting pyresticd Backup')
-    
+
     # run restic
     args = [restic_command] + restic_args.split()
 
@@ -73,7 +73,7 @@ if answer and answer[0].lower() == 'y':
 
 # Scheduling
 
-schedule.every(day_interval).day.at(backup_at).do(do_restic_backup, restic_password)
+schedule.every(day_interval).days.at(backup_at).do(do_restic_backup, restic_password)
 
 while True:
     schedule.run_pending()
