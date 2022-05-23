7d9d06563f841a1a5c01507eb0bfb5d83e200ed0
Mebus <mebus@mailaix.de>
2022-05-23 22:11:10 +0200

    use indirect output

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -5,11 +5,15 @@ import urllib.request
 import json
 import datetime
 import dateutil.parser
+import sys
 
 
 class PyresticDMonitor:
 
     failure = False
+
+    def aprint(self, text):
+        self.ascii += text + "\n"
 
     def ascii_art(self):
         return """
@@ -31,20 +35,23 @@ class PyresticDMonitor:
             when = dateutil.parser.isoparse(data["when"])
             time_delta = (datetime.datetime.utcnow() - when).days
 
-            print(data["backup_name"])
-            print("%s days ago" % str(time_delta))
+            self.ascii += data["backup_name"] + "\n"
+            self.ascii += "%s days ago\n" % str(time_delta)
             if data["success"] and (time_delta <= float(data["day_interval"])):
-                print("OK")
+                self.ascii += "OK\n"
             else:
                 if ho["ignore"]:
-                    print("fail (ignored)")
+                    self.ascii += "fail (ignored)\n"
                 else:
                     self.failure = True
-                    print("FAIL")
+                    self.ascii += "FAIL\n"
 
-            print("-" * 30)
+            self.aprint("-" * 30)
 
     def run(self):
+
+        self.ascii = ""
+        self.aprint(self.ascii_art())
 
         f = open("inventory.json")
         data = json.load(f)
@@ -57,9 +64,12 @@ class PyresticDMonitor:
 
 if __name__ == "__main__":
     pm = PyresticDMonitor()
-
-    print(pm.ascii_art())
     pm.run()
+    print(pm.ascii)
 
     if pm.failure:
         print("\nWARNING: at least one backup >>> HAS FAILED <<<.\n")
+        sys.exit(2)
+
+    else:
+        sys.exit(0)
