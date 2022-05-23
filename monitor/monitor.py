964d8d017b4c3288066eecc3d05aacd84efa3ef3
Mebus <mebus@mailaix.de>
2022-05-23 20:06:54 +0200

    ran black on monitor.py

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -5,8 +5,8 @@ import json
 import datetime
 import dateutil.parser
 
-class PyresticDMonitor:
 
+class PyresticDMonitor:
     def process_host(self, url):
 
         with urllib.request.urlopen(url) as remote:
@@ -16,27 +16,27 @@ class PyresticDMonitor:
 
             print(data["backup_name"])
             print("%s days ago" % str(time_delta))
-            if(data["success"] and (time_delta <= float(data["day_interval"]))):
+            if data["success"] and (time_delta <= float(data["day_interval"])):
                 print("OK")
             else:
                 print("FAIL")
 
-            print("-"*30)
+            print("-" * 30)
 
     def run(self):
 
-        f = open('inventory.json')
+        f = open("inventory.json")
         data = json.load(f)
         for ho in data["hosts"]:
             self.process_host(ho["url"])
-  
+
         # Closing file
         f.close()
 
+
 if __name__ == "__main__":
 
-    print("="*30)
-    pm = PyresticDMonitor();
+    print("=" * 30)
+    pm = PyresticDMonitor()
     pm.run()
-    print("="*30)
-
+    print("=" * 30)
