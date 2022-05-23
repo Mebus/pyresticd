a68f6a4017c38a4ba4052b23a101df8ef1f5fb9e
Mebus <mebus.inbox@googlemail.com>
2022-05-23 07:14:12 +0200

    black

--- pyresticd.py
+++ pyresticd.py
@@ -11,16 +11,17 @@ import syslog
 
 # Configuration
 
-restic_password = ''
+restic_password = ""
 
 # Load Configuration
 
 config = configparser.ConfigParser()
 config.read("pyresticd.cfg")
-backup_at = config['pyresticd']['backup_at']
-day_interval = int(config['pyresticd']['day_interval'])
+backup_at = config["pyresticd"]["backup_at"]
+day_interval = int(config["pyresticd"]["day_interval"])
 
 # Program
+
 
 def logthis(msg):
 
@@ -33,48 +34,58 @@ def logthis(msg):
 def do_restic_backup(password):
 
     # Log start
-    logthis('Starting pyresticd Backup')
+    logthis("Starting pyresticd Backup")
 
-    restic_args = "backup " + config['pyresticd']['src_dir'] + " --exclude-file excludes.txt --cache-dir " + config['restic']['cache'] + " --exclude-caches"
+    restic_args = (
+        "backup "
+        + config["pyresticd"]["src_dir"]
+        + " --exclude-file excludes.txt --cache-dir "
+        + config["restic"]["cache"]
+        + " --exclude-caches"
+    )
 
     # run restic
-    args = [config['restic']['binary']] + restic_args.split()
+    args = [config["restic"]["binary"]] + restic_args.split()
 
-    ps = subprocess.Popen(args, env={
-        'B2_ACCOUNT_ID' : config['B2']['B2_ACCOUNT_ID'],
-        'B2_ACCOUNT_KEY' : config['B2']['B2_ACCOUNT_KEY'],
-        'RESTIC_PASSWORD': password,
-        'RESTIC_REPOSITORY': config['pyresticd']['repo'],
-        'PATH': os.environ['PATH'],
-    })
+    ps = subprocess.Popen(
+        args,
+        env={
+            "B2_ACCOUNT_ID": config["B2"]["B2_ACCOUNT_ID"],
+            "B2_ACCOUNT_KEY": config["B2"]["B2_ACCOUNT_KEY"],
+            "RESTIC_PASSWORD": password,
+            "RESTIC_REPOSITORY": config["pyresticd"]["repo"],
+            "PATH": os.environ["PATH"],
+        },
+    )
 
     ps.wait()
 
-    #Log finish
-    logthis('Finished pyresticd Backup')
+    # Log finish
+    logthis("Finished pyresticd Backup")
 
 
-print('=' * 30)
-print('Restic Scheduler')
-print('=' * 30)
-print("Repository: " + config['pyresticd']['repo'])
+print("=" * 30)
+print("Restic Scheduler")
+print("=" * 30)
+print("Repository: " + config["pyresticd"]["repo"])
 print("backup at: " + backup_at)
 print("day interval: " + str(day_interval))
 
-print('-' * 30)
+print("-" * 30)
 
-if not restic_password and 'RESTIC_PASSWORD' in os.environ:
-    restic_password = os.environ['RESTIC_PASSWORD']
+if not restic_password and "RESTIC_PASSWORD" in os.environ:
+    restic_password = os.environ["RESTIC_PASSWORD"]
 
 if not restic_password:
     restic_password = getpass.getpass(
-        prompt='Please enter the restic encryption password: ')
+        prompt="Please enter the restic encryption password: "
+    )
     print("Password entered.")
 
 # Immediate Backup
 
-answer = input('Run a backup just now? [y/n]')
-if answer and answer[0].lower() == 'y':
+answer = input("Run a backup just now? [y/n]")
+if answer and answer[0].lower() == "y":
     do_restic_backup(restic_password)
 
 # Scheduling
